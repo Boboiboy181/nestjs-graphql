f74865a05effee3b631ce3cd781e7d447517accd
Gia Hai <haidao549@gmail.com>
2024-01-29 10:12:51 +0700

    create lesson entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Lesson } from './lesson/lesson.entity';
 import { LessonModule } from './lesson/lesson.module';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb+srv://haidao549:haideptrai@clusters.m14nthm.mongodb.net/?retryWrites=true&w=majority',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [Lesson],
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       autoSchemaFile: true,
       driver: ApolloDriver,
